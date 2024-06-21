e354e2dc4e5ee6994dae5d8b3f3cd906388f2a06
claudiobzrr <claudio.bezerra1998@gmail.com>
2024-06-20 22:31:01 -0300

    🚀 First Commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'prettier/prettier',
+    'prettier/react',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
---
+++ src/Game/GameBoard.tsx
@@ -0,0 +1,5 @@
+export function GameBoard() {
+  return (
+    <canvas id="game-board" className="w-[350px]  sm:w-[600px] h-[350px] sm:h-[600px] bg-green-600 rounded-xl"></canvas>
+  );
+}
